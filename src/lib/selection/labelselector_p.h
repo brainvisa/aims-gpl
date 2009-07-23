94740bfe703fa6bdbbbbceaac494f32108797a54
denisri <denis.riviere.gm@gmail.com>
2009-07-23 18:13:06 +0000

    integrate
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/perforce/aims-gpl/trunk@33519 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- src/lib/selection/labelselector_p.h
+++ src/lib/selection/labelselector_p.h
@@ -71,7 +71,11 @@ protected:
 
 protected slots:
   void deleteSelection();
+#if QT_VERSION >= 0x040000
   void rightButtonPressed( Q3ListViewItem*, const QPoint & pos, int );
+#else
+  void rightButtonPressed( QListViewItem*, const QPoint & pos, int );
+#endif
 
 private:
   aims::gui::LabelSelector	*_labelsel;
